05a7668ab1bffc901c745f3fc0cc1f11e9f040e4
yancey <yancey1989@gmail.com>
2024-03-05 18:58:19 +0800

    update

--- tao_compiler/mlir/custom_ops/transpose_impl.cc
+++ tao_compiler/mlir/custom_ops/transpose_impl.cc
@@ -69,7 +69,6 @@ TAO_RAL_API("ral_transpose", "gpu", ral_transpose<Eigen::half, 3>);
 TAO_RAL_API("ral_transpose", "gpu", ral_transpose<Eigen::bfloat16, 2>);
 TAO_RAL_API("ral_transpose", "gpu", ral_transpose<Eigen::bfloat16, 3>);
 TAO_RAL_API("ral_transpose", "gpu", ral_transpose<bool, 2>);
->>>>>>> update
 #endif
 
 }  //  namespace ral
